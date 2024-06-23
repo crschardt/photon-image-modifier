6942e77926aef19b21887b102b40140691ae6653
Craig Schardt <crschardt@fastem.com>
2024-06-23 07:45:54 -0500

    just run the install script

--- install_opi5.sh
+++ install_opi5.sh
@@ -17,31 +17,31 @@ chmod +x install.sh
 
 sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=4-7/g' install.sh
 
-./install.sh
+./install.sh -q
 rm install.sh
 
 
-# Remove extra packages 
-echo "Purging extra things"
-apt-get remove -y gdb gcc g++ linux-headers* libgcc*-dev
-apt-get remove -y snapd
-apt-get autoremove -y
+# # Remove extra packages 
+# echo "Purging extra things"
+# apt-get remove -y gdb gcc g++ linux-headers* libgcc*-dev
+# apt-get remove -y snapd
+# apt-get autoremove -y
 
 
-echo "Installing additional things"
-sudo apt-get update
-apt-get install -y network-manager net-tools libatomic1
-# mrcal stuff
-apt-get install -y libcholmod3 liblapack3 libsuitesparseconfig5
-apt-get install -y libc6 libstdc++6
+# echo "Installing additional things"
+# sudo apt-get update
+# apt-get install -y network-manager net-tools libatomic1
+# # mrcal stuff
+# apt-get install -y libcholmod3 liblapack3 libsuitesparseconfig5
+# apt-get install -y libc6 libstdc++6
 
-cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
-network:
-  renderer: NetworkManager
-EOF
+# cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
+# network:
+#   renderer: NetworkManager
+# EOF
 
-rm -rf /var/lib/apt/lists/*
-apt-get clean
+# rm -rf /var/lib/apt/lists/*
+# apt-get clean
 
-rm -rf /usr/share/doc
-rm -rf /usr/share/locale/
+# rm -rf /usr/share/doc
+# rm -rf /usr/share/locale/
