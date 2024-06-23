b9315d38872624cb2d4c52ca559fb21d270ebb09
Craig Schardt <crschardt@fastem.com>
2024-06-23 17:43:59 -0500

    re-enable the pi account

--- install_opi5.sh
+++ install_opi5.sh
@@ -4,7 +4,7 @@ if id "$1" >/dev/null 2>&1; then
     echo 'user found'
 else
     echo "creating pi user"
-    useradd pi -b /home
+    useradd pi -m -b /home -s /bin/bash
     usermod -a -G sudo pi
     mkdir /home/pi
     chown -R pi /home/pi
@@ -17,19 +17,19 @@ chmod +x install.sh
 
 sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=4-7/g' install.sh
 
-./install.sh -m -q
+./install.sh -n -q
 rm install.sh
 
-# Installing addtional things
-apt-get install -y network-manager net-tools libatomic1
-# mrcal stuff
-apt-get install -y libcholmod3 liblapack3 libsuitesparseconfig5
-apt-get install -y libc6 libstdc++6
+# # Installing addtional things
+# apt-get install -y network-manager net-tools libatomic1
+# # mrcal stuff
+# apt-get install -y libcholmod3 liblapack3 libsuitesparseconfig5
+# apt-get install -y libc6 libstdc++6
 
-cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
-network:
-  renderer: NetworkManager
-EOF
+# cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
+# network:
+#   renderer: NetworkManager
+# EOF
 
 # # Remove extra packages 
 # echo "Purging extra things"
