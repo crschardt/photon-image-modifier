41f9978022ff0d5035211cbb74abbfe51f07afbc
Craig Schardt <crschardt@fastem.com>
2024-07-14 21:53:08 -0500

    leave wait online (for now)

--- install_opi5.sh
+++ install_opi5.sh
@@ -70,7 +70,7 @@ sed -i 's/-n$//' /etc/systemd/system/photonvision.service
 cat /etc/systemd/system/photonvision.service
 
 # systemctl disable NetworkManager-wait-online.service
-systemctl disable systemd-networkd-wait-online.service
+# systemctl disable systemd-networkd-wait-online.service
 
 # the bluetooth service isn't needed and causes a delay at boot
 systemctl disable ap6275p-bluetooth.service
