989c668ef2af5fa4fcde829c0c85de13bcbe8e1b
Craig Schardt <crschardt@fastem.com>
2025-11-09 10:13:51 -0600

    mount, unmount and resize the image

--- test_install.sh
+++ test_install.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 # set -uo pipefail
 
+rootdir="./rootfs"
+
 url=$1
 additional_mb=$2
 rootpartition=$3
+
+if [[ $# -ge 4 ]]; then
+    bootpartition=$4
+    if [[ "x$rootpartion" = "x$bootpartition" ]]; then
+        echo "Boot partition cannot be equal to root partition"
+        exit 1
+    fi
+else
+    bootpartition=
+fi
 
 # Install required packages
 # sudo apt-get update
 # sudo apt-get install -y wget xz-utils
 
+####
+# Download and prepare the image
+####
 wget -nv -O base_image.img.xz "${url}"
 xz -T0 -d base_image.img.xz
 
 ls -sh base_image.img
+
+####
+# Download and mount the image
+####
 
 if [[ ${additional_mb} -gt 0 ]]; then
     dd if=/dev/zero bs=1M count=${additional_mb} >> base_image.img
@@ -21,12 +40,13 @@ fi
 ls -sh base_image.img
 
 loopdev=$(losetup --find --show --partscan base_image.img)
+# echo "loopdev=${loopdev}" >> $GITHUB_OUTPUT
 
-echo "Before resize"
-lsblk ${loopdev}
+part_type=$(blkid -o value -s PTTYPE "${loopdev}")
+echo "Image is using ${part_type} partition table"
 
 if [[ ${additional_mb} -gt 0 ]]; then
-    if ( (parted --script $loopdev print || false) | grep "Partition Table: gpt" > /dev/null); then
+    if [[ "${part_type}" == "gpt" ]]; then
         sgdisk -e "${loopdev}"
     fi
     parted --script "${loopdev}" resizepart ${rootpartition} 100%
@@ -37,9 +57,93 @@ fi
 
 sync
 
-echo "After resize"
-lsblk ${loopdev}
+lsblk "${loopdev}"
+partprobe -s "${loopdev}"
+if [[ -n "$bootpartition" ]]; then
+    bootdev="${loopdev}p{$bootpartition}"
+else
+    bootdev=
+fi
+rootdev="${loopdev}p{$rootpartition}"
 
+mkdir --parents ${rootdir}
+echo "rootdir=${rootdir}" >> "$GITHUB_OUTPUT"
+mount "${rootdev}" "${rootdir}"
+if [[ -n "$bootpartition" ]]; then
+    mkdir --parents "${rootdir}/boot"
+    mount "${bootdev}" "${rootdir}/boot"
+else
+    bootdev=
+fi
+
+mv "${rootdir}/etc/resolv.conf" "${rootdir}/etc/resolv.conf.bak"
+cp /etc/resolv.conf "${rootdir}/etc/resolv.conf"
+
+####
+# Mdify the image in chroot
+####
+
+
+
+####
+# Clean up and shrink image
+####
+
+if [[ -e "${rootdir}/etc/resolv.conf" ]]; then
+    mv "${rootdir}/etc/resolv.conf.bak" "${rootdir}/etc/resolv.conf"
+fi
+
+echo "Before zero filling free space"
+df -H
+
+if [[ -d "${rootdir}/boot" ]]; then
+    (cat /dev/zero > "${rootdir}/boot/zeros" 2>/dev/null); sync; rm "${rootdir}/boot/zeros";
+    # umount "${rootdir}/boot"
+fi
+(cat /dev/zero > "${rootdir}/zeros" 2>/dev/null); sync; rm "${rootdir}/zeros";
+# umount "${rootdir}"
+
+echo "After zero filling free space"
+df -H
+
+if [[ -d "${rootdir}/boot" ]]; then
+    umount "${rootdir}/boot"
+fi
+umount "${rootdir}"
+
+echo "Resizing root filesystem to minimal size."
+e2fsck -v -f -p -E discard "${rootdev}"
+resize2fs -M "${rootdev}"
+rootfs_blocksize=$(tune2fs -l ${rootdev} | grep "^Block size" | awk '{print $NF}')
+rootfs_blockcount=$(tune2fs -l ${rootdev} | grep "^Block count" | awk '{print $NF}')
+
+echo "Resizing rootfs partition."
+rootfs_partstart=$(parted -m --script "${loopdev}" unit B print | grep "^${rootpartition}:" | awk -F ":" '{print $2}' | tr -d 'B')
+rootfs_partsize=$((${rootfs_blockcount} * ${rootfs_blocksize}))
+rootfs_partend=$((${rootfs_partstart} + ${rootfs_partsize} - 1))
+rootfs_partoldend=$(parted -m --script "${loopdev}" unit B print | grep "^${rootpartition}:" | awk -F ":" '{print $3}' | tr -d 'B')
+if [ "$rootfs_partoldend" -gt "$rootfs_partend" ]; then
+    echo y | parted ---pretend-input-tty "${loopdev}" unit B resizepart "${rootpartition}" "${rootfs_partend}"
+else
+    echo "Rootfs partition not resized as it was not shrunk"
+fi
+
+free_space=$(parted -m --script "${loopdev}" unit B print free | tail -1)
+if [[ "${free_space}" =~ "free" ]]; then
+    initial_image_size=$(stat -L --printf="%s" "${image}")
+    image_size=$(echo "${free_space}" | awk -F ":" '{print $2}' | tr -d 'B')
+    if [[ "${part_type}" == "gpt" ]]; then
+        # for GPT partition table, leave space at the end for the secondary GPT 
+        # it requires 33 sectors, which is 16896 bytes
+        image_size=$((image_size + 16896))
+    fi            
+    echo "Shrinking image from ${initial_image_size} to ${image_size} bytes."
+    truncate -s "${image_size}" "${image}"
+    if [[ "${part_type}" == "gpt" ]]; then
+        # use sgdisk to fix the secondary GPT after truncation 
+        sgdisk -e "${image}"
+    fi
+fi
 
 losetup --detach "${loopdev}"
 
