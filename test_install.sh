77d4ced18bbfe6876d9d54d81069219781fac068
Craig Schardt <crschardt@fastem.com>
2025-11-08 19:24:11 -0600

    stop confusing url for image

--- test_install.sh
+++ test_install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # set -uo pipefail
 
-image=$1
+url=$1
 additional_mb=$2
 
 # Install required packages
 # sudo apt-get update
 # sudo apt-get install -y wget xz-utils
 
-wget -nv -O base_image.img.xz "${image}"
+wget -nv -O base_image.img.xz "${url}"
 xz -T0 -d base_image.img.xz
 
 ls
 
 if [[ ${additional_mb} -gt 0 ]]; then
-    dd if=/dev/zero bs=1M count=${additional_mb} >> ${image}
+    dd if=/dev/zero bs=1M count=${additional_mb} >> base_image.img.xz
 fi
 
 loopdev=$(losetup --find --show --partscan base_image.img)
